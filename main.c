0690a8ab7d32167f6dbb26328550f47ceffa5447
Crispin <chipres@uci.edu>
2021-04-22 20:33:28 -0700

    updated and added more comments

--- main.c
+++ main.c
@@ -2,47 +2,50 @@
 #define GPIODATA *((unsigned int *) 0x400253FCU)
 #define GPIODIR  *((unsigned int *) 0x40025400U)
 #define GPIODEN  *((unsigned int *) 0x4002551CU)
-/* 
-    GPIO Run Mode Clock Gating Control (RCGCGPIO)
-    GPIO Direction                     (GPIODIR)
-    GPIO Digital Enable                (GPIODEN)
-    
-     
+#define RED             0x02U
+#define BLUE            0x04U
+#define PURPLE          0x06U    
+#define GREEN           0x08U
+#define YELLOW          0x0AU
+#define LIGHTBLUE       0x0CU
+#define WHITE           0x0EU
 
-*/
 void delay(int lim){
-  int count = 0;
+  int volatile count = 0;
   while( count < lim){
     count++;
   }
 }
+
+
 int main()
 {
+  /*  GPIO Run Mode Clock Gating Control (RCGCGPIO) 
+      set register to 0x20U to enable GPIO Port F in Run mode (pg 340) */
    RCGCGPIO = 0x20U;
+   
+  /*  GPIO Direction                     (GPIODIR)  
+      set register to 0x0EU to configure pins 1,2,3 as an output (pg 663)*/
    GPIODIR  = 0x0EU;
+   
+  /*  GPIO Digital Enable                (GPIODEN)  
+      set register to 0x0EU to configure pins 1,2,3 to eneable (pg 682)*/
+   
    GPIODEN  = 0x0EU;
    
-   unsigned int color_arr[] = { 0x02U, 0x04U, 0x06U, 0x08U, 0x0AU, 0x0CU, 0x0EU};
+   unsigned int color_arr[] = { RED, BLUE, PURPLE, GREEN, YELLOW, LIGHTBLUE, WHITE};
 
   int i = 0; 
+  
   while(1){
   
-    /*
-    GPIODATA = 0x08U;
-    delay(10000000);
-     
-    GPIODATA = 0x0eU;
-    delay(10000000);
-    */
     i = i % 7;
     GPIODATA = color_arr[i];
     delay(1500000);
     i++;
       
   }
+  
   return 0;
   
-  /* clock gate */
-  /* GPIO Direction (GPIODIR) set to out offset 0x0400*/
-  /* GPIO Digital Enable (GPIODEN) offset 0x051C*/
 }
